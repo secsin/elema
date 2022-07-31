6e9bc6aee12d82f756fc2067bf4fbbd861d1bd83
secsin <32921301+secsin@users.noreply.github.com>
2022-07-31 23:54:19 +0800

    complete simple components

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,85 +1,88 @@
-var path = require('path')
-var config = require('../config')
-var utils = require('./utils')
-var projectRoot = path.resolve(__dirname, '../')
+var path = require("path");
+var config = require("../config");
+var utils = require("./utils");
+var projectRoot = path.resolve(__dirname, "../");
 
 module.exports = {
   entry: {
-    app: './src/main.js'
+    app: "./src/main.js",
   },
   output: {
     path: config.build.assetsRoot,
-    publicPath: process.env.NODE_ENV === 'production' ? config.build.assetsPublicPath : config.dev.assetsPublicPath,
-    filename: '[name].js'
+    publicPath:
+      process.env.NODE_ENV === "production"
+        ? config.build.assetsPublicPath
+        : config.dev.assetsPublicPath,
+    filename: "[name].js",
   },
   resolve: {
-    extensions: ['', '.js', '.vue'],
-    fallback: [path.join(__dirname, '../node_modules')],
+    extensions: ["", ".js", ".vue"],
+    fallback: [path.join(__dirname, "../node_modules")],
     alias: {
-      'src': path.resolve(__dirname, '../src'),
-      'assets': path.resolve(__dirname, '../src/assets'),
-      'components': path.resolve(__dirname, '../src/components')
-    }
+      src: path.resolve(__dirname, "../src"),
+      assets: path.resolve(__dirname, "../src/assets"),
+      components: path.resolve(__dirname, "../src/components"),
+    },
   },
   resolveLoader: {
-    fallback: [path.join(__dirname, '../node_modules')]
+    fallback: [path.join(__dirname, "../node_modules")],
   },
   module: {
     preLoaders: [
       {
         test: /\.vue$/,
-        loader: 'eslint',
+        loader: "eslint",
         include: projectRoot,
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.js$/,
-        loader: 'eslint',
+        loader: "eslint",
         include: projectRoot,
-        exclude: /node_modules/
-      }
+        exclude: /node_modules/,
+      },
     ],
     loaders: [
       {
         test: /\.vue$/,
-        loader: 'vue'
+        loader: "vue",
       },
       {
         test: /\.js$/,
-        loader: 'babel',
+        loader: "babel",
         include: projectRoot,
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.json$/,
-        loader: 'json'
+        loader: "json",
       },
       {
         test: /\.html$/,
-        loader: 'vue-html'
+        loader: "vue-html",
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url',
+        loader: "url",
         query: {
           limit: 10000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
-        }
+          name: utils.assetsPath("img/[name].[hash:7].[ext]"),
+        },
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url',
+        loader: "url",
         query: {
           limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
-        }
-      }
-    ]
+          name: utils.assetsPath("fonts/[name].[hash:7].[ext]"),
+        },
+      },
+    ],
   },
   eslint: {
-    formatter: require('eslint-friendly-formatter')
+    formatter: require("eslint-friendly-formatter"),
   },
   vue: {
-    loaders: utils.cssLoaders()
-  }
-}
+    loaders: utils.cssLoaders(),
+  },
+};
--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
-import Vue from 'vue'
-import App from './App'
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+import app from "./App";
+import goods from "./components/goods/goods";
+import ratings from "./components/ratings/ratings";
+import seller from "./components/seller/seller";
 
 /* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
-})
+// new Vue({
+//   el: "body",
+//   components: { app },
+// });
+
+Vue.use(VueRouter);
+let router = new VueRouter();
+router.map({
+  "/goods": {
+    component: goods,
+  },
+  "/ratings": {
+    component: ratings,
+  },
+  "/seller": {
+    component: seller,
+  },
+});
+// 启动路由
+router.start(app, "#app");
+// 请求默认路由
+router.go("/goods");
